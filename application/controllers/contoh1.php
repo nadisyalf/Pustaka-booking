458c896af28ef59f2d19af1e158bd7db1c274e9a
nadisyalf <nadisyalma15@gmail.com>
2021-10-19 12:06:35 +0700

    Ex : Tampil Biodata

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+    echo "<h1>Perkenalankan</h1>"
+    echo "Nama saya Nadisya Alma 
+        Saya tinggal di daerah bekasi
+        Beberapa minggu ini saya banyak mendengarkan musik tahun 90an
+        salah satunya adalah lagu dari MLTR"
+    }
+}
+   
