e0db154e6d41b4c62c7bdf52971ba13dd89004a6
nadisyalf <nadisyalma15@gmail.com>
2021-10-19 13:24:33 +0700

    Latihan1-Pertemuan3

---
+++ application/controllers/latihan1.php
@@ -0,0 +1,16 @@
+<?php
+class latihan1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "Selamat Datang..
+                Salmat belajar Web Programing";
+        //$this->load->view('view-latihan1');
+    }
+    public penjumlahan($n1, $n2)
+    {
+        $this->load->model('Model_latihan1')
+        $hasil =$this->Model_latihan1->jmulah($n1,n2);
+        echo "jhasil Penjumlahan dari".$n1."+".$n2."="
+    }
+}
