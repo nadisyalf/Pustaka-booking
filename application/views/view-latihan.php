4856b20ab25a971c3f32260467f96e45ba1637ca
nadisyalf <nadisyalma15@gmail.com>
2021-10-28 18:12:01 +0700

    contoh 2

---
+++ application/views/view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk Kita belajar web programing..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+
+Ini hasil dari pemodelaan dengan methode penjumlahan yaitu <?=nilai."+".$nilai2."=".$hasdil;?>
+
+</body>
+</html>
